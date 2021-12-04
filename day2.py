fa7cc114b7d4ed2445a9dfa099febd97d5305b12
jr <jr@jirouette.com>
2021-12-04 11:15:29 +0100

    day2 part2

--- day2.py
+++ day2.py
@@ -18,6 +18,24 @@ def part1(filename: str) -> Tuple[int, int]:
                     Y -= int(y)
     return X, Y
 
+def part2(filename: str) -> Tuple[int, int]:
+    X = 0
+    Y = 0
+    aim = 0
+    with open(filename) as f:
+        for line in f:
+            match line.split():
+                case ["forward", x]:
+                    X += int(x)
+                    Y += aim*int(x)
+                case ["down", y]:
+                    aim += int(y)
+                case ["up", y]:
+                    aim -= int(y)
+    return X, Y
+
 if __name__ == '__main__':
     X, Y = part1("inputs/day2.txt")
     print("part1", X, "×", Y, "=", X*Y)
+    X, Y = part2("inputs/day2.txt")
+    print("part2", X, "×", Y, "=", X*Y)
