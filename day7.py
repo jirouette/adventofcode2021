efb3a6f48ca4bbd25a3d6101714f190ccc991af6
jr <jr@jirouette.com>
2021-12-12 13:34:27 +0100

    day7 part2

--- day7.py
+++ day7.py
@@ -1,6 +1,12 @@
 #!/usr/bin/python
 #coding: utf8
 
+def sum_fact(nb: int) -> int:
+    result = 0
+    for i in range(nb+1):
+        result += i
+    return result
+
 def part1(filename: str) -> int:
     fuel_cost = []
     with open(filename) as f:
@@ -15,5 +21,20 @@ def part1(filename: str) -> int:
         fuel_cost.append(fuel)
     return min(fuel_cost)
 
+def part2(filename: str) -> int:
+    fuel_cost = []
+    with open(filename) as f:
+        positions = [int(pos) for pos in f.read().split(',')]
+    for i in range(max(positions)+1):
+        fuel = 0
+        for pos in positions:
+            if pos > i:
+                fuel += sum_fact(pos - i)
+            else:
+                fuel += sum_fact(i - pos)
+        fuel_cost.append(fuel)
+    return min(fuel_cost)
+
 if __name__ == '__main__':
     print("part1", part1("inputs/day7.txt"))
+    print("part2", part2("inputs/day7.txt"))
