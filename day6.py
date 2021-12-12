df805a5aad431f187a0510db6d5c69e3a14418f8
jr <jr@jirouette.com>
2021-12-12 13:16:18 +0100

    day6 part2

--- day6.py
+++ day6.py
@@ -15,5 +15,20 @@ def part1(filename: str, nb_days: int) -> int:
             state.append(8)
     return len(state)
 
+def part2(filename: str, nb_days: int) -> int:
+    state = {i: 0 for i in range(9)}
+    with open(filename) as f:
+        for fish in f.read().split(','):
+            state[int(fish)] += 1
+    for day in range(nb_days):
+        next = 0
+        for key in list(range(9))[::-1]:
+            next, state[key] = state[key], next
+        state[8] += next
+        state[6] += next
+    return sum(state.values())
+
+
 if __name__ == '__main__':
     print("part1", part1("inputs/day6.txt", 80))
+    print("part2", part2("inputs/day6.txt", 256))
