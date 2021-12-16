3a25f3795dfb9895a1164cf4e2c8c66fa74eac5d
jr <jr@jirouette.com>
2021-12-16 17:41:28 +0100

    day10 part2

--- day10.py
+++ day10.py
@@ -19,5 +19,29 @@ def part1(filename: str) -> int:
                     break
     return score
 
+PART2_SCORE = {')': 1, ']': 2, '}': 3, '>': 4}
+def part2(filename: str) -> int:
+    scores = []
+    with open(filename) as f:
+        for line in f:
+            expecting = []
+            for c in line[:-1]:
+                if expecting and c == expecting[-1]:
+                    expecting.pop()
+                elif c in MATCHING.keys():
+                    expecting.append(MATCHING[c])
+                else:
+                    print("Expecting", expecting[-1], "received", c)
+                    break
+            else:
+                line_score = 0
+                for e in expecting[::-1]:
+                    line_score *= 5
+                    line_score += PART2_SCORE[e]
+                scores.append(line_score)
+    scores.sort()
+    return scores[int(len(scores)/2)]
+
 if __name__ == '__main__':
     print("part1", part1("inputs/day10.txt"))
+    print("part2", part2("inputs/day10.txt"))
